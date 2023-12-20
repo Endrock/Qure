2c54a48ce126e8cb92851db2b6f570337ade34af
Juan <montoj@gmail.com>
2023-12-20 17:34:49 -0500

    Initial: social proof data added on PDP

--- assets/endrock.js
+++ assets/endrock.js
@@ -1,1 +1,66 @@
 console.log('all Endrock js scripts should be here');
+
+document.addEventListener('DOMContentLoaded', function() {
+
+  // Start PDP Social Proof Data
+  const socialProofContainer = document.querySelector('.social-proof-data-container');
+  const quantityElement = document.querySelector('.social-proof-data-container #quantity');
+  const closeIcon = document.querySelector('.social-proof-data-container .close-icon');
+  const containerAtcButton = document.querySelector('.nav_e-commerce .cart-custom');
+
+  if ( socialProofContainer && document.body.hasAttribute('data-social-proof')) {
+    // let themePreviewId = Shopify.theme.role == "development" ? `&preview_theme_id=${Shopify.theme.id}` : '';
+    let { productId, productHandle, productList } = socialProofContainer.dataset; 
+    let url = `https://webhooks.endrock.software/endrockapi/v3/app/analytics/reportsGA4.php?filterBy=productId&store=qure&name=Qure: GA4&productId=`;
+    
+    const renderQuantity = (quantity) => { 
+      quantityElement.innerText = quantity;
+      if (containerAtcButton) containerAtcButton.setAttribute('style', "bottom: 44px !important"); 
+      socialProofContainer.classList.remove('hidden');
+    }
+
+    if ( productHandle && productList ) {
+      let arrProductList = productList.split(', ');
+      const requests = arrProductList.map(id => {
+        return fetchData(url, id);
+      });
+      Promise.all(requests)
+        .then(responses => {
+          let sumPurchased = responses.reduce((acum,cur)=>{
+            return acum + cur.data.itemsPurchased; 
+          },0);
+          if ( sumPurchased > 0) {
+            renderQuantity(sumPurchased);
+          }
+        })
+        .catch(error => console.log('error', error));
+    } else if ( productId )  {
+      fetchData(url, productId)
+        .then(response => {
+          if ( response.code == 200 ) {
+            renderQuantity(response.data.itemsPurchased);
+          }
+        })
+    }
+
+    function fetchData (url, id) {
+      let requestOptions = {
+        method: 'GET',
+        redirect: 'follow'
+      };
+
+      return fetch(url+id, requestOptions)
+        .then(response => response.json())
+        .catch(error => console.log('error', error));
+    }
+  }
+
+  closeIcon.addEventListener('click', function () {
+    socialProofContainer.classList.add('hidden');
+    containerAtcButton.setAttribute('style', "bottom: 25px !important"); 
+  });
+  // End PDP Social Proof Data
+
+});
+
+
